d734360b41441787f800bbdec60279ff88cbea70
Thinh Nguyen D <thinh.coding@gmail.com>
2024-07-18 16:24:46 +0700

    updates

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var flkty = new Flickity(".main-carousel", {
   // options
 });
 
+// Varieties
 const BASE_URL = "https://steam-api-dot-cs-platform-306304.et.r.appspot.com";
 const genreList = document.querySelector(".genre-list");
 
@@ -20,6 +21,7 @@ var requestOption = {
   redirect: "follow",
 };
 
+//Functions
 const getGenreList1 = async () => {
   try {
     const url = BASE_URL + "/genres?page=1";
@@ -59,13 +61,56 @@ const getGenreList3 = async () => {
     return [];
   }
 };
+const getRecommendedGame = async () => {
+  try {
+    const url = BASE_URL + "/features";
+    const response = await fetch(url, requestOption);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log("error by getting data of recommended game", error);
+  }
+};
 
+//RenderUI
 const renderUIGenreList = (genreListData) => {
   genreListData.forEach((genre) => {
     const btnGenre = document.createElement("div");
     btnGenre.className = "btnGenre";
     btnGenre.innerHTML = genre.name;
     genreList.appendChild(btnGenre);
+  });
+};
+const renderUIRecommendedGame = (recommendedGameData) => {
+  recommendedGameData.forEach((game) => {
+    const recommendedGame = document.createElement("div");
+    recommendedGame.className = "bestGame-container";
+    recommendedGame.innerHTML = `
+              <div class="bestGame-img">
+                <img
+                  src="${game["header_image"]}"
+                  alt="${game.name}"
+                />
+              </div>
+              <div class="bestGame-info">
+                <div class="bestGame-title">${game.name}</div>
+                <div class="bestGame-genre">
+                ${game.genres.map(
+                  (genre) => `<div class="genre-tag">${genre}</div>`
+                )}
+                </div>
+                <div class="bestGame-default">Now Available</div>
+                <div class="bestGame-price">${game.price}</div>
+                <div class="bestGame-platform">
+                ${game.platforms.map(
+                  (platform) => `<div class="platform-tag">${platform}</div>`
+                )}
+                </div>
+              </div>
+    `;
+    document.querySelector(".main-carousel").appendChild(recommendedGame);
   });
 };
 
@@ -96,63 +141,21 @@ const renderGenreList3 = async () => {
     return [];
   }
 };
-
 const renderGenreList = () => {
   renderGenreList1();
   renderGenreList2();
   renderGenreList3();
 };
-
-renderGenreList();
-
-const getRecommendedGame = async () => {
-  try {
-    const url = BASE_URL + "/features";
-    const response = await fetch(url, requestOption);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    }
-  } catch (error) {
-    console.log("error by getting data of recommended game", error);
-  }
-};
-
 const renderRecommendedGame = async () => {
   try {
     const response = await getRecommendedGame();
-    response.data.forEach((game) => {
-      const recommendedGame = document.createElement("div");
-      recommendedGame.className = "bestGame-container";
-      recommendedGame.innerHTML = `
-                <div class="bestGame-img">
-                  <img
-                    src="${game["header_image"]}"
-                    alt="${game.name}"
-                  />
-                </div>
-                <div class="bestGame-info">
-                  <div class="bestGame-title">${game.name}</div>
-                  <div class="bestGame-genre">
-                  ${game.genres.map(
-                    (genre) => `<div class="genre-tag">${genre}</div>`
-                  )}
-                  </div>
-                  <div class="bestGame-default">Now Available</div>
-                  <div class="bestGame-price">${game.price}</div>
-                  <div class="bestGame-platform">
-                  ${game.platforms.map(
-                    (platform) => `<div class="platform-tag">${platform}</div>`
-                  )}
-                  </div>
-                </div>
-      `;
-      document.querySelector(".main-carousel").appendChild(recommendedGame);
-    });
+    renderUIRecommendedGame(response.data);
   } catch (error) {
     console.log("error from render recommended game", error);
     return [];
   }
 };
 
+//Actions
+renderGenreList();
 renderRecommendedGame();
